4267899ea31e985e9132df4a692d105d7168a31a
hemasathwika <135261406+hemasathwika@users.noreply.github.com>
2024-04-25 22:34:39 +0530

    basic commits

---
+++ bookstore/frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import '../src/components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>pratyusha</h1>
+      <home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ bookstore/frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to My React App</h1>
+      <p>This is the home page of my React app.</p>
+      <p>You can start building your app from here!</p>
+    </div>
+  );
+};
+
+export default Home;
